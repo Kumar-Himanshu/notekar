f80bcccea928ca292b76ebb121d4a23d7c9cd022
KHimanshuSingh <KHimanshu@ustechsolutions.com>
2020-08-11 15:15:46 +0530

    image converstion in progress

--- app/src/main/java/com/notekar/SharePictureFragment.kt
+++ app/src/main/java/com/notekar/SharePictureFragment.kt
@@ -2,6 +2,7 @@ package com.notekar
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.net.Uri
@@ -12,6 +13,7 @@ import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.notekar.interfaces.IOnBackPressed
 import com.notekar.utils.Constants
 import com.notekar.utils.Utility
 import java.io.File
@@ -19,13 +21,15 @@ import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 
+
 /**
  * A simple [Fragment] subclass.
  * Use the [SharePictureFragment] factory method to
  * create an instance of this fragment.
  */
-class SharePictureFragment : Fragment() {
+class SharePictureFragment : Fragment(), IOnBackPressed {
     private val args: SharePictureFragmentArgs by navArgs()
+
     private lateinit var bitmap: Bitmap
     private lateinit var imageView: AppCompatImageView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +58,11 @@ class SharePictureFragment : Fragment() {
     }
 
     private fun initUI() {
-        bitmap = textAsBitmap(args.bitmap, 18.0f, resources.getColor(R.color.app_base_color))!!
-        imageView.setImageBitmap(bitmap)
+        imageView.post(Runnable {
+            bitmap = textAsBitmap(args.bitmap, 100.0f, resources.getColor(R.color.app_base_color),imageView)!!
+            imageView.setImageBitmap(bitmap)
+        })
+//        imageView.setImageBitmap(bitmap)
     }
 
     private fun shareAsImage(imagePath: Uri) {
@@ -85,11 +92,15 @@ class SharePictureFragment : Fragment() {
             fos.flush()
             fos.close()
         } catch (e: FileNotFoundException) {
-            Log.e("GREC", e.localizedMessage, e)
+            Log.e("SharePictureFragment", e.localizedMessage, e)
         } catch (e: IOException) {
-            Log.e("GREC", e.localizedMessage, e)
+            Log.e("SharePictureFragment", e.localizedMessage, e)
         }
-        return FileProvider.getUriForFile(requireContext(), requireContext().applicationContext.packageName + ".provider", imagePath)
+        return FileProvider.getUriForFile(
+            requireContext(),
+            requireContext().applicationContext.packageName + ".provider",
+            imagePath
+        )
 //        return Uri.fromFile(imagePath)
     }
 
@@ -103,23 +114,55 @@ class SharePictureFragment : Fragment() {
         }
     }
 
-    private fun textAsBitmap(text: String?, textSize: Float, textColor: Int): Bitmap? {
-        var width = 400 // round
-        var height = 400
-//        imageView.post {
-//            width = imageView.width
-//            height = imageView.height
-//        }
+    private fun textAsBitmap(text: String?, textSize: Float, textColor: Int, imageView:View): Bitmap? {
+//        val options = BitmapFactory.Options()
+//        options.inJustDecodeBounds = true
+//        val bitmap = BitmapFactory.decodeResource(resources, R.id.imgView, options)
+//        val imageHeight = options.outHeight
+//        val imageWidth = options.outWidth
+//        val imageType = options.outMimeType
+        val imageHeight = imageView.height
+//        val imageWidth = imageView.width
         val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         paint.textSize = textSize
         paint.color = textColor
         paint.textAlign = Paint.Align.LEFT
         val baseline: Float = -paint.ascent() // ascent() is negative
-//        val width = (paint.measureText(text) + 0.5f).toInt() // round
-//        val height = (baseline + paint.descent() + 0.5f).toInt()
-        val image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val width = (paint.measureText(text) + 0.5f).toInt() // round
+//        var height = (paint.measureText(text)  + 0.5f).toInt() // round
+////        var height = (baseline + paint.descent() + 0.5f).toInt()
+//        if(height < 400){
+//            height = 400
+//        }
+        val image = Bitmap.createBitmap(width, imageHeight, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(image)
         canvas.drawText(text!!, 0.0f, baseline, paint)
-        return image
+        val newBitmap = resize(image, image.width / 2, 1800)
+        return newBitmap
+    }
+
+    override fun onBackPressed(): Boolean {
+        return false
+    }
+
+    private fun resize(image: Bitmap, maxWidth: Int, maxHeight: Int): Bitmap? {
+        var image = image
+        return if (maxHeight > 0 && maxWidth > 0) {
+            val width = image.width
+            val height = image.height
+            val ratioBitmap = width.toFloat() / height.toFloat()
+            val ratioMax = maxWidth.toFloat() / maxHeight.toFloat()
+            var finalWidth = maxWidth
+            var finalHeight = maxHeight
+            if (ratioMax > ratioBitmap) {
+                finalWidth = (maxHeight.toFloat() * ratioBitmap).toInt()
+            } else {
+                finalHeight = (maxWidth.toFloat() / ratioBitmap).toInt()
+            }
+            image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true)
+            image
+        } else {
+            image
+        }
     }
 }
